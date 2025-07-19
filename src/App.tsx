d1b3d4a283758659c7ac4a48ea8c1a936d2dc5c8
xieisabug <136419808@qq.com>
2025-07-19 20:52:24 +0800

    chore: 删除PreviewHTMLWindow组件及相关引用

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.css';
 import AskWindow from "./AskWindow.tsx";
 import ConfigWindow from "./ConfigWindow.tsx";
 import ChatUIWindow from './ChatUIWindow.tsx';
-import PreviewHTMLWindow from './artifacts/PreviewHTMLWindow.tsx';
 import ArtifactPreviewWindow from './artifacts/ArtifactPreviewWindow.tsx';
 import PluginWindow from './PluginWindow.tsx';
 import { Toaster } from './components/ui/sonner.tsx';
@@ -12,7 +11,6 @@ const windowsMap: Record<string, typeof AskWindow> = {
     ask: AskWindow,
     config: ConfigWindow,
     chat_ui: ChatUIWindow,
-    preview_html: PreviewHTMLWindow,
     artifact_preview: ArtifactPreviewWindow,
     plugin: PluginWindow
 }
